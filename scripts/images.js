7ab1325b2cdb6e5370b75ecb9de302e94ea4f294
TheCalculus <seven.four.six.five@proton.me>
2025-03-08 16:00:06 +1100

    finalise analysis images, add new image for conclusion, add new image for analysis3, add raw data and graph spreadsheet link to sources, slightly change css

--- scripts/images.js
+++ scripts/images.js
@@ -1,7 +1,6 @@
 const images = document.getElementsByTagName("img");
-let image;
 for (let i = 0; i < images.length; i++) {
-    image = images[i];
+    const image = images[i];
     image.addEventListener("click", () => {
         window.open(image.src, "_blank").focus();
     });
